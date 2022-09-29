33b70732ac1cb632fde91c7ae957310a19510cc5
bondarus <bondarus@tcd.ie>
2022-09-29 14:51:53 +0100

    Get user input string to array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an expression you want to calculate: ");
+        String str = input.nextLine();
+        Pattern p = Pattern.compile("[*,+,-]+|\\d+");
+        Matcher m = p.matcher(str);
+        ArrayList<String> allMatches = new ArrayList<>();
+        while(m.find())
+        {
+            allMatches.add(m.group());
+        }
+        System.out.println(Arrays.toString(allMatches.toArray()));
     }
 }
