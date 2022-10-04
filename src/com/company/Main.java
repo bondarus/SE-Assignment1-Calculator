3a73f521a088be8a580b519627394b33194af119
bondarus <bondarus@tcd.ie>
2022-10-04 19:32:59 +0100

    Made calculations in a function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,6 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        int currentVal = 0;
-        int i = 0;
-        boolean exit1 = false;
-        boolean exit2 = false;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter an expression you want to calculate: ");
         String str = input.nextLine();
@@ -27,77 +23,7 @@ public class Main {
         boolean valid = isValid(allMatches);
         if(valid)
         {
-            System.out.println("The expression is valid");
-            while(!exit1 && i < allMatches.size())
-            {
-                if((allMatches.get(i)).equals("*") && allMatches.size() >= 3)
-                {
-                    int val1 = Integer.parseInt(allMatches.get(i-1));
-                    int val2 = Integer.parseInt(allMatches.get(i+1));
-                    currentVal = val1 * val2;
-                    System.out.println("\n" + val1 + " * " + val2 + " = " + currentVal);
-                    System.out.println("Current Value: " + currentVal);
-                    if(allMatches.size() > 3) {
-                        allMatches.set(i,Integer.toString(currentVal));
-                        allMatches.remove(i+1);
-                        allMatches.remove(i-1);
-                        System.out.println("Current Size: " + allMatches.size());
-                        System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
-                        i = -1;
-                    }
-                    else {
-                        exit1 = true;
-                    }
-                    i++;
-                }
-                i++;
-            }
-            i = 0;
-            while(!exit2)
-            {
-                if((allMatches.get(i)).equals("+") && allMatches.size() >= 3)
-                {
-                    int val1 = Integer.parseInt(allMatches.get(i-1));
-                    int val2 = Integer.parseInt(allMatches.get(i+1));
-                    currentVal = val1 + val2;
-                    System.out.println("\n" + val1 + " + " + val2 + " = " + currentVal);
-                    System.out.println("Current Value: " + currentVal);
-                    if(allMatches.size() > 3) {
-                        allMatches.set(i,Integer.toString(currentVal));
-                        allMatches.remove(i+1);
-                        allMatches.remove(i-1);
-                        System.out.println("Current Size: " + allMatches.size());
-                        System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
-                        i = -1;
-                    }
-                    else {
-                        exit2 = true;
-                    }
-                    i++;
-                }
-                else if((allMatches.get(i)).equals("-") && allMatches.size() >= 3)
-                {
-                    int val1 = Integer.parseInt(allMatches.get(i-1));
-                    int val2 = Integer.parseInt(allMatches.get(i+1));
-                    currentVal = val1 - val2;
-                    System.out.println("\n" + val1 + " - " + val2 + " = " + currentVal);
-                    System.out.println("Current Value: " + currentVal);
-                    if(allMatches.size() > 3) {
-                        allMatches.set(i,Integer.toString(currentVal));
-                        allMatches.remove(i+1);
-                        allMatches.remove(i-1);
-                        System.out.println("Current Size: " + allMatches.size());
-                        System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
-                        i = -1;
-                    }
-                    else {
-                        exit2 = true;
-                    }
-                    i++;
-                }
-                i++;
-            }
-            System.out.println("Result: " + currentVal);
+            calculate(allMatches);
         }
         else
         {
@@ -135,6 +61,85 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static void calculate(ArrayList<String> allMatches)
+    {
+        int currentVal = 0;
+        int i = 0;
+        boolean exit1 = false;
+        boolean exit2 = false;
+        System.out.println("The expression is valid");
+        while(!exit1 && i < allMatches.size())
+        {
+            if((allMatches.get(i)).equals("*") && allMatches.size() >= 3)
+            {
+                int val1 = Integer.parseInt(allMatches.get(i-1));
+                int val2 = Integer.parseInt(allMatches.get(i+1));
+                currentVal = val1 * val2;
+                System.out.println("\n" + val1 + " * " + val2 + " = " + currentVal);
+                System.out.println("Current Value: " + currentVal);
+                if(allMatches.size() > 3) {
+                    allMatches.set(i,Integer.toString(currentVal));
+                    allMatches.remove(i+1);
+                    allMatches.remove(i-1);
+                    System.out.println("Current Size: " + allMatches.size());
+                    System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
+                    i = -1;
+                }
+                else {
+                    exit1 = true;
+                }
+                i++;
+            }
+            i++;
+        }
+        i = 0;
+        while(!exit2)
+        {
+            if((allMatches.get(i)).equals("+") && allMatches.size() >= 3)
+            {
+                int val1 = Integer.parseInt(allMatches.get(i-1));
+                int val2 = Integer.parseInt(allMatches.get(i+1));
+                currentVal = val1 + val2;
+                System.out.println("\n" + val1 + " + " + val2 + " = " + currentVal);
+                System.out.println("Current Value: " + currentVal);
+                if(allMatches.size() > 3) {
+                    allMatches.set(i,Integer.toString(currentVal));
+                    allMatches.remove(i+1);
+                    allMatches.remove(i-1);
+                    System.out.println("Current Size: " + allMatches.size());
+                    System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
+                    i = -1;
+                }
+                else {
+                    exit2 = true;
+                }
+                i++;
+            }
+            else if((allMatches.get(i)).equals("-") && allMatches.size() >= 3)
+            {
+                int val1 = Integer.parseInt(allMatches.get(i-1));
+                int val2 = Integer.parseInt(allMatches.get(i+1));
+                currentVal = val1 - val2;
+                System.out.println("\n" + val1 + " - " + val2 + " = " + currentVal);
+                System.out.println("Current Value: " + currentVal);
+                if(allMatches.size() > 3) {
+                    allMatches.set(i,Integer.toString(currentVal));
+                    allMatches.remove(i+1);
+                    allMatches.remove(i-1);
+                    System.out.println("Current Size: " + allMatches.size());
+                    System.out.println("Current List: " + Arrays.toString(allMatches.toArray()));
+                    i = -1;
+                }
+                else {
+                    exit2 = true;
+                }
+                i++;
+            }
+            i++;
+        }
+        System.out.println("Result: " + currentVal);
     }
 
     public void test(){
